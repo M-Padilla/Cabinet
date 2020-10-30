4198939b1e442adb2a1b062b68bb08c2c7509ca4
M-Padilla <M-Padilla@users.noreply.github.com>
2020-10-30 14:09:51 -0500

    Update SOATPDFParser.py
    -PDF conversion to image is now executed completely in memory, not using temporary files and folders
    -Personalized offset for cropbox has been implemented as a deterministic way to get fields with different positions among insurers.
    -Field position absolute coordinates uploaded as Numpy array

--- SOAT/SOATPDFParser.py
+++ SOAT/SOATPDFParser.py
@@ -1,29 +1,20 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Oct 10 15:25:16 2020
 
-@author: user
+@author: M.Padilla
 """
 
 from pdf2image import convert_from_path
 import pytesseract
 import numpy as np
 import cv2 as cv
-import tempfile
 
 
-#File name
-'''
-filename= "SOAT Sura 2.pdf" 
-fd = open(filename, "rb")
-
-pages = convert_from_path("C:/Users/user/Documents/Proyectos/Cabinet 1/SOAT Bolivar.pdf",
-                          dpi=150,
-                          fmt="png", 
-                          output_folder="C:/Users/user/Documents/Proyectos/Cabinet 1",
-                          last_page=1)
-'''
-
+def box_construction(fields_array):
+    fields_array[:,2]+= fields_array[:,0]
+    fields_array[:,3]+= fields_array[:,1]
+    return fields_array
+    
 #Visualize image
 def test():
     cv.namedWindow('dmc',cv.WINDOW_NORMAL)
@@ -34,101 +25,108 @@ def test():
     cv.waitKey(1)
 
 
-# Convert to .jpeg image file using a temporary file.
-with tempfile.TemporaryDirectory() as path:
-    images_from_path = convert_from_path("C:/Users/user/Documents/Proyectos/Cabinet 1/SOAT CARDENAS.pdf",
+
+#Offset coordinates (+X,+Y) dictionary for cropbox, customized for each insurer.
+policy_offset={"AXA COLPATRIA": (0,5),
+               "BOLIVAR": (0,-5),
+               "MUNDIAL": (0,0),
+               "SURA": (0,120)}
+
+    
+
+pytesseract.pytesseract.tesseract_cmd = 'D:/Programas/anaconda3/pkgs/tesseract-4.1.1-h1fd39ab_3/Library/bin/tesseract.exe'
+
+# Convert to .jpeg image file.
+images_from_path = convert_from_path("C:/Users/user/Documents/Proyectos/Cabinet 1/SOAT CARDENAS.pdf",
                                          fmt="jpeg",
                                          dpi=400,
-                                         output_folder=path,
                                          single_file=True)
     
+for image in images_from_path:
+    #Convert to a NumPy array with RGB values for reading through OpenCV
+    open_cv_image= np.array(image)
+    #Crop image to a box delimited by (Y,X) to remove footer and blank spaces in the top and sides (y:y+h, x:x+w)
+    cropbox= (200,-1780, 200, -200)
+    insurer_name = "SURA" #TO-DO: Extend to dilate for Sura, to compensate for thinner font.
+    offset= policy_offset.get(insurer_name,(0,0))
+    cropbox= (cropbox[0]+offset[1], cropbox[1]+offset[1], cropbox[2]+offset[0], cropbox[3]+offset[0]) 
+    open_cv_image= open_cv_image[cropbox[0]:cropbox[1], cropbox[2]:cropbox[3]]
+    # Convert RGB to BGR (required by OpenCV) gray scale.
+    open_cv_image = cv.cvtColor(open_cv_image,cv.COLOR_BGR2GRAY)     
+    #Threshold evaluation: If pixel value is greater than 180, pixel value becomes 255 (white).
+    ret,open_cv_image = cv.threshold(open_cv_image,130,255,cv.THRESH_BINARY)
+    #Blur correction, to remove high frequency content (eg: noise, edges) from the image
+    open_cv_image = cv.GaussianBlur(open_cv_image, (7,7), sigmaX=0, sigmaY=0)  
+    cv.imwrite('ASTRALDO.jpeg',open_cv_image)               
+                
+    
+    #Numpy array containing all regions of interest, cropped from open_cv_image.
+    #(x,y,w,h)  
+    fields= np.array([[2000,1150,925,120],
+                    [940,1350,845,120],
+                    [1705,1960,555,435],
+                    [2300,1940,220,450],
+                    [2145,950,370,120],
+                    [1100,570,315,80],
+                    [740,1960,440,110],
+                    [640,570,325,75],
+                    [2465,0,480,530],
+                    [110,1740,540,120],
+                    [186,570,340,75],
+                    [445,1185,1545,85],
+                    [445,1100,1545,85],
+                    [2530,950,390,120],
+                    [110,1350,810,115],
+                    [110,1150,160,120],
+                    [520,950,335,120],
+                    [110,950,370,120],
+                    [2515,1540,410,120],
+                    [110,1540,1135,120],
+                    [2085,1570,420,95],
+                    [1690,1570,380,90],
+                    [1265,1540,410,120],
+                    [105,1950,150,120],
+                    [1190,1960,465,110],
+                    [1470,950,660,120],
+                    [275,1960,445,110],
+                    [2665,1350,255,120],
+                    [83,2264,450,98],
+                    [870,950,590,120],
+                    [1800,1350,845,120]])    
+    
+    
+    #Dictionary containing the fields index row belonging to each policy field.
+    fields_dict = {"body": 0,
+            "chassis_serial": 1,
+            "coverage": 2, #Per victim
+            "coverage_limits": 3, #Limits given as number of daily minimum legal wages.
+            "cylinder_cap": 4,
+            "expiration_date": 5, #All policies expire at 23:59 of the given day.
+            "fosyga_fee": 6,
+            "inception_date": 7,
+            "insurer": 8,
+            "insurer_code": 9,
+            "issue_date": 10, #All policies expire at 00:00 of the given day.
+            "line": 11,
+            "manufacturer": 12,
+            "model_year": 13,
+            "motor_serial": 14,
+            "passenger_cap": 15,
+            "plate": 16,
+            "policy_number": 17,
+            "policyholder_city": 18,
+            "policyholder_fullname": 19,
+            "policyholder_id": 20,
+            "policyholder_id_class": 21,
+            "policyholder_phone": 22,
+            "pricing_rate_code": 23,
+            "runt_fee": 24,
+            "service_class": 25,
+            "soat_premium": 26,
+            "ton_cap": 27,
+            "total_premium": 28,
+            "vehicle_class": 29,
+            "vin": 30}
+    
     for image in images_from_path:
-        #Convert to a NumPy array with RGB values for reading through OpenCV
-        open_cv_image= np.array(image)
-        #Crop image to remove footer and blank spaces in the top and sides (y:y+h, x:x+w)
-        open_cv_image= open_cv_image[100:-1800,200:-200] 
-        open_cv_image = cv.bitwise_not(open_cv_image)
-        im= open_cv_image.copy()
-        # Convert RGB to BGR (required by OpenCV) gray scale.
-        open_cv_image = cv.cvtColor(open_cv_image,cv.COLOR_BGR2GRAY)
-        #Blur correction, to remove high frequency content (eg: noise, edges) from the image
-        open_cv_image = cv.GaussianBlur(open_cv_image, (9,9), 0)  
-        #Threshold evaluation: If pixel value is greater than 180, pixel value becomes 255 (white).
-        ret,open_cv_image = cv.threshold(open_cv_image,130,255,cv.THRESH_BINARY)
-        cv.imwrite('CSTRALDO.jpeg',open_cv_image)
-        '''     
-        kernel = cv.getStructuringElement(cv.MORPH_RECT, (14,2))
-        open_cv_image = cv.dilate(open_cv_image, kernel, iterations=4)
-        '''
-        kernel = cv.getStructuringElement(cv.MORPH_RECT, (15,7))
-        open_cv_image = cv.dilate(open_cv_image, kernel, iterations=4)
-        cv.imwrite('ASTRALDO.jpeg',open_cv_image)
-        open_cv_image= cv.Canny(open_cv_image, 0, 255)
-        
-        
-        _, contours, hierarchy = cv.findContours(open_cv_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        drawing = np.zeros((open_cv_image.shape[0], open_cv_image.shape[1], 3), dtype=np.uint8)
-        for i in range(len(contours)):
-            color = (255, 0, 255)
-            cv.drawContours(drawing, contours, i, color, 2, cv.LINE_8, hierarchy, 0)
-        
-        test()
-
-        
-        '''
-        #Dictionary containing all regions of interest, cropped from open_cv_image.
-        fields = {"body": (2003, 1206,  927,  153),
-                "chassis_serial": (940, 1396,  833,  166),
-                "coverage": (1703, 2009,  530,  459),
-                "coverage_limit": (2296, 1990,  654,  500),
-                "cylinder_cap": (2150,  996,  356,  172),
-                "expiration_date": (993,  628,  423,  153),
-                "fosyga_fee": (733, 2003,  447,  159),
-                "inception_date": (553,  606,  413,  169),
-                "insurer": (2336,   59,  664,  600),
-                "insurer_code": (110, 1796,  540,  163),
-                "issue_date": (190,  600,  326,  156),
-                "line": (283, 1293, 1703,   75),
-                "manufacturer": (283, 1209, 1697, 75),
-                "model_year": (2533, 1000,  380,  165),
-                "motor_serial": (113, 1409,  793,  153),
-                "passenger_cap": (106, 1203,  157,  162),
-                "plate": (520, 1006,  323,  162),
-                "policy_number": (105,  987,  403,  188),
-                "policyholder_city": (2506, 1593,  440,  169),
-                "policyholder_fullname": (116, 1587, 1134,  169),
-                "policyholder_id": (2083, 1587,  410,  175),
-                "policyholder_id_class": (1690, 1587,  373,  169),
-                "policyholder_phone": (1263, 1587,  407,  178),
-                "pricing_rate_code": (106, 2006,  150,  156),
-                "runt_fee": (1190, 2000,  466,  168),
-                "service_class": (1476, 1006,  640,  165),
-                "soat_premium": (280, 2003,  443,  172),
-                "ton_cap": (2670, 1396,  246,  160),
-                "total_premium": (83, 2184,  357,  178),
-                "vehicle_class": (876, 1006,  570,  159),
-                "vin": (1803, 1400,  837,  165)}
-        
-        key="total_premium"
-        cv.imwrite('BSTRALDO.jpeg',open_cv_image[fields[key][1]:fields[key][1]+fields[key][3],
-                                                 fields[key][0]:fields[key][0]+fields[key][2]])
-        '''
-        
-        '''
-        fromCenter = False
-        cv.namedWindow('Image',cv.WINDOW_NORMAL)
-        cv.resizeWindow('Image', 900,800)
-        r = cv.selectROIs("Image", open_cv_image, fromCenter)
-        cv.destroyWindow('Image')
-        '''
-        
-        
-        
-    for image in images_from_path:
-        image.close()
-
-'''
-c = line_items_coordinates[10]
-img = open_cv_image[c[0][1]:c[1][1], c[0][0]:c[1][0]]
-cv.imshow('dmc', img); cv.waitKey(0); cv.destroyAllWindows(); cv.waitKey(1)
-'''
+      image.close()
